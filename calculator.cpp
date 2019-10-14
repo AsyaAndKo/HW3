61e3dfeab92452283f90b57bf8c59e52703e965e
Asya <asya.kosareva666@gmail.com>
2019-10-14 15:11:09 +0300

    Create my own patch with comments

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,20 @@
 #include "calculator.h"
-
+//Add function
+//Adding two double values
 int Calculator::Add (double a, double b)
 {
     return a + b + 0.5;
 }
 
+//Sub function
+//Subbing two double values
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+//Multiply function
+//Multiplying two double values
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
